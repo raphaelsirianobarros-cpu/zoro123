b14f24f5e192f9ba7f7e14b0911a8b28edf1d4d2
raixzx15-droid <raphaelsirianobarros@gmail.com>
2026-06-19 20:30:50 -0300

    muito legal
    fizemos uma calculadora
    fizemos uma barra de caregamento

---
+++ 0010/script.js
@@ -0,0 +1,11 @@
+var  x = 2;
+var y = 5;
+var resutado = x + y;
+var sesutado = x - y;
+var resutado = x / y;
+var resutado = x * y;
+console.log(`A soma de x e y é $
+    {resutado}´ )
+var num1 = Windows.prompt(`digite outro numero: ´)
+var num2
+var 
---
+++ ativ9/script.js
@@ -0,0 +1,8 @@
+window.alert(`ola mundo`)
+console.log(`ola mundo - console `)
+Window.prompt(Digite seu nome: `)
+let paragrafo = document.getElementById(`texto`)
+console.log(paragrafo)
+paragrafo.innerHTML = `<p> Ola Mundo Receba o $ { nome} como novo jogador </p>`  
+paragrafo.style.color = `red`
+
